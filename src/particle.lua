bcae51fdc43165f619f290dff482a1bee0b4c821
xdarkzero <Traxypad@gmail.com>
2013-08-25 15:00:14 -0600

    played with particle effects

--- src/particle.lua
+++ src/particle.lua
@@ -5,28 +5,25 @@ function initParticles ()
    burstImg = love.graphics.newImage("image/twin-part.png")
 
    burst = {}
-   burst.scale = 1 
+   burst.scale = 1
    burst.width = burstImg:getWidth()
    burst.height = burstImg:getHeight()
    -- laser burst: set bunch of properties for it
-   burst.particle = love.graphics.newParticleSystem(burstImg, 10)
+   burst.particle = love.graphics.newParticleSystem(burstImg, 100)
    burst.particle:setLifetime(1)
    burst.particle:setSpread(math.rad(360)) -- in radians! this bug was not fun to track down
    burst.particle:setEmissionRate(100)
    burst.particle:setSpeed(100, 200)
    burst.particle:setGravity(0)
-   burst.particle:setSizes(2, 1)
-   burst.particle:setColors(255, 255, 255, 255, 58, 128, 255, 0)
+   burst.particle:setSizes(3, 5)
+   burst.particle:setColors(255, 255, 255, 255, 0, 153, 255, 0)
 --   burst.particle:setColors(220, 105, 20, 255, 194, 30, 18, 0) -- fire particle with part1.png
-   burst.particle:setLifetime(0.25)
+   burst.particle:setLifetime(0.5)
    burst.particle:setParticleLife(0.5)
---   burst.particle:setLifetime(1)
---   burst.particle:setParticleLife(1)
-   burst.particle:setSpin(1)
-   burst.particle:setSpinVariation(2,1)
-   -- neat spinning effects with stuff like this
---   burst.particle:setRadialAcceleration(-2000)
---   burst.particle:setTangentialAcceleration(1000) 
+   burst.particle:setSpin(math.pi/2)
+   burst.particle:setSpinVariation(0,1)
+   burst.particle:setRadialAcceleration(-1000)
+--   burst.particle:setTangentialAcceleration(-100) 
    burst.particle:stop()
 
    particles.burst = burst
